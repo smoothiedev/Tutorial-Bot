28fecbf0ef084e2a3cf275eaa42038d20a5fa9b9
York <aarghgames@gmail.com>
2016-11-15 00:34:47 +0000

    Updated app.js
    End result after episode 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('./settings.json');
+const token = require('./settings.json').token;
 
 client.on('ready',() => {
 	console.log('I\'m Online\nI\'m Online');
@@ -8,10 +8,37 @@ client.on('ready',() => {
 
 var prefix = "~"
 client.on('message', message => {
-	if (message.author === client.user) return;
+	let args = message.content.split(' ').slice(1);
+	var result = args.join(' ');
+
+	if (!message.content.startsWith(prefix)) return;
+	if (message.author.bot) return;
+
 	if (message.content.startsWith(prefix + 'ping')) {
-		message.channel.sendMessage('pong');
+		message.channel.sendMessage(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
+	} else
+
+	if (message.content.startsWith(prefix + 'send')) {
+		client.channels.get('245491978601627648').sendMessage('Hello from second channel!');
+	} else
+
+	if (message.content.startsWith(prefix + 'setgame')) {
+		if (!result) {
+			result = null;
+		}
+		client.user.setGame(result);
+	} else
+
+	if (message.content.startsWith(prefix + 'setstatus')) {
+		if (!result) {
+			result = 'online';
+		}
+		client.user.setStatus(result);
+	} else
+
+	if (message.content.startsWith(prefix + 'foo')) {
+		message.channel.sendMessage('bar');
 	}
 });
 
-client.login(settings.token);
+client.login(token);
