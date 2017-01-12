0c46239d86682c390f0c8453af96d9df0b59f4a8
York <aarghgames@gmail.com>
2017-01-12 11:23:05 +0000

    Changes after Episode 6 part 2

--- events/message.js
+++ events/message.js
@@ -1,17 +1,22 @@
 const settings = require('../settings.json');
 module.exports = message => {
-	if (!message.content.startsWith(settings.prefix)) return;
+  if (!message.content.startsWith(settings.prefix)) return;
   if (message.author.bot) return;
+  let client = message.client;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(settings.prefix)) return;
+  let command = message.content.split(' ')[0].slice(settings.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
 
-	const client = message.client;
-	const args = message.content.split(' ');
-	const command = args.shift().slice(settings.prefix.length);
-
-
-	try {
-		let cmdFile = require(`../commands/${command}`);
-		cmdFile.run(client, message, args);
-	} catch (err) {
-		console.log(`Command ${command} failed\n${err.stack}`);
-	}
 };
